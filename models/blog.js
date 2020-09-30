42b1a0b060f1da9e29d8c185493874b6800560f2
aditsanputra <aditsanputra@gmail.com>
2020-09-30 20:12:08 +0700

    Added basic mongodb

---
+++ models/blog.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const blogSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    snippet: {
+        type: String,
+        required: true
+    },
+    body: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true });
+
+//Blog = singular of Blogs which is the name of the collection in the database
+const Blog = mongoose.model('Blog', blogSchema);
+module.exports = Blog;
